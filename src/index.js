457b027d93731ca0255fa3a2ef6d8a8a05d0446e
oolenkazolot <oolenka.zolot@gmail.com>
2022-06-25 11:55:29 +0200

    feat: create reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    if (n > 0) {
+        let string = String(n);
+        let stringRevers = string.split("");
+        let c = stringRevers[2];
+        if (c === "0") {
+            delete stringRevers[2];
+        }
+        stringRevers = stringRevers.reverse().join("");
+        let number = Number(stringRevers);
+        return number;
+    } else if (n < 0) {
+        let string = String(n);
+        let stringRevers = string.split("").reverse().join("").slice(0, -1);
+        let number = Number(stringRevers);
+        return number;
+    }
+};
